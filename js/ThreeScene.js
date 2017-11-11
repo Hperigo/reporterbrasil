d39832df4ca7accf7145d39df230e4412b92bd3b
hperigo <hperigo@gmail.com>
2017-11-11 12:33:02 -0200

    3d model fixes

--- js/ThreeScene.js
+++ js/ThreeScene.js
@@ -1,3 +1,15 @@
+function shortAngleDist(a0,a1) {
+    var max = Math.PI*2;
+    var da = (a1 - a0) % max;
+    return 2*da % max - da;
+}
+
+function angleLerp(a0,a1,t) {
+    return a0 + shortAngleDist(a0,a1)*t;
+}
+
+
+
 function SelectableObject(){
 	this.object = null;
 	this.domObject = null;
@@ -10,9 +22,10 @@ function SelectableObject(){
 
 	}
 
-	this.setup = function( obj, html_info, id, onclick ){
+	this.setup = function( obj, html_info, id, onclick, sceneRef ){
 
 		// var title = 
+
 		
 		var content = "\
 		<div class='content-description'> \
@@ -29,10 +42,11 @@ function SelectableObject(){
 		var title_el = $(
 		"\
 		<div class='object-selector'> \
+		 <img class='content-picker' width=70 height=70 src='point.svg' type='image/svg+xml'></img> \
 		<h1> <span class=selector-id>#" + id + "</span> " + html_info.name  + "</h1>\
 		</div>"
 		);
-			 
+		
 
 
 		$(title_el).attr('id','selector_' + id);  // set the attribute 
@@ -42,14 +56,20 @@ function SelectableObject(){
 
 		title_el.append( content_el );
 
+		var passObj = this;
 
-		title_el.click( function(){
+		function onClickCallback(){
+			onclick(content_el, passObj, this.threeObj);
+		}
 
-			onclick(content_el);
-		} )
+		title_el.click( onClickCallback.bind(this) );
 
+		this.isActive = false;
 		this.domObject = title_el
 		this.object = obj;
+		this.threeObj = sceneRef
+		this.targetOffset = html_info.offset
+		
 	}
 }
 
@@ -98,15 +118,20 @@ function ThreeScene(){
 
 		// SetupCamera & controls
 		this.camera = new THREE.PerspectiveCamera( 40, width / height, 1, 1800 )
-		this.camera.filmOffset = -7
+		this.camera.filmOffset = -5
 		this.camera.updateProjectionMatrix()
-		this.camera.position.x = -23.523036313168493
-		this.camera.position.y = 13.429693441816438
-		this.camera.position.z = -5.073035185092259
+		// this.camera.position.x = -23.523036313168493
+		// this.camera.position.y = 13.429693441816438
+		// this.camera.position.z = -5.073035185092259
+
+		this.camera.position.set( 10.426024262307237, 3.100505513233176, 6.5711637018614395 )
 
 		this.controls = new THREE.OrbitControls( this.camera, domElement  );
-		// this.controls.maxPolarAngle = 1.394867138193868;
-		// this.controls.minPolarAngle = 1.1058406140636055;
+		this.controls.maxPolarAngle = 1.394867138193868;
+		this.controls.minPolarAngle = 1.1058406140636055;
+
+		this.controls.minDistance = 6;
+		this.controls.maxDistance = 13;
 
 		this.controls.enableDamping = true;
 		this.controls.dampingFactor = 0.05;
@@ -119,7 +144,9 @@ function ThreeScene(){
 
 		// this.controls.target.x = 3
 
-		var texture = new THREE.TextureLoader().load( "Baked_big2.jpg" );
+		var texBig = new THREE.TextureLoader().load( "big.jpg" );
+		var texMed = new THREE.TextureLoader().load( "medium.jpg" );
+		var texSmall = new THREE.TextureLoader().load( "small.jpg" );
 		
 
 
@@ -130,7 +157,8 @@ function ThreeScene(){
 		var _isInitialized = this.isInitialized;
 
 
-		var helperAxis = new THREE.AxisHelper(1);
+		var helperAxis = new THREE.Object3D();
+		helperAxis.visible = true;
 		this.scene.add(helperAxis);
 
 		this.controlOrigin = helperAxis;
@@ -145,32 +173,38 @@ function ThreeScene(){
 				{
 					name : 'Porto de Suape',
 					subtitle: 'Despejo na ilha de Tatuoca',
-					description: 'Lorem ipsum dolor sit amet, consectetuer Lorem ipsum dolor sit amet'
+					description: 'Lorem ipsum dolor sit amet, consectetuer Lorem ipsum dolor sit amet',
+					offset: -1.6595359678142114
 				},
 				{
 					name : 'Termelétricas',
 					subtitle: 'Despejo na ilha de Tatuoca',
-					description: 'Lorem ipsum dolor sit amet, consectetuer Lorem ipsum dolor sit amet'
+					description: 'Lorem ipsum dolor sit amet, consectetuer Lorem ipsum dolor sit amet',
+					offset:-3.099817026147467
 				},
 				{
 					name : 'A Comunidade',
 					subtitle: 'Despejo na ilha de Tatuoca',
-					description: 'Lorem ipsum dolor sit amet, consectetuer Lorem ipsum dolor sit amet'
+					description: 'Lorem ipsum dolor sit amet, consectetuer Lorem ipsum dolor sit amet',
+					offset: 1.4494059994729995
 				},
 				{
 					name : 'Vila Claudete',
 					subtitle: 'Despejo na ilha de Tatuoca',
-					description: 'Lorem ipsum dolor sit amet, consectetuer Lorem ipsum dolor sit amet'
+					description: 'Lorem ipsum dolor sit amet, consectetuer Lorem ipsum dolor sit amet',
+					offset: 0.32726812978316466
 				},
 				{
 					name : 'Complexo <br> <span style="left:10px;"> Industrial </spam>',
 					subtitle: 'Despejo na ilha de Tatuoca',
-					description: 'Lorem ipsum dolor sit amet, consectetuer Lorem ipsum dolor sit amet'
+					description: 'Lorem ipsum dolor sit amet, consectetuer Lorem ipsum dolor sit amet',
+					offset: -0.29655149102431366
 				},
 				{
 					name : 'Quilombo e ameaças',
 					subtitle: 'Despejo na ilha de Tatuoca',
-					description: 'Lorem ipsum dolor sit amet, consectetuer Lorem ipsum dolor sit amet'
+					description: 'Lorem ipsum dolor sit amet, consectetuer Lorem ipsum dolor sit amet',
+					offset: -0.851654833113707
 				}
 			]
 
@@ -187,8 +221,18 @@ function ThreeScene(){
 
 				var mat = new THREE.MeshBasicMaterial();
 
-				if(object.children[i].name == 'big')  
-					mat.map = texture;
+				if(object.children[i].name == 'big'){
+					mat.map = texBig;
+				}
+
+				else if(object.children[i].name == 'medium'){
+					mat.map = texMed;
+				}
+
+				else if(object.children[i].name == 'small'){
+					mat.map = texSmall;
+				}
+
 
 				var color = new THREE.Color();
 				var r = Math.random();
@@ -197,14 +241,51 @@ function ThreeScene(){
 
 				object.children[i].material = mat
 
-				if(i != 0 ){ // ignore floor plane
+				var objectName = object.children[i].name.split('_');
+	
+				if(  objectName[1] == 'mark' ){ // ignore floor plane
+
+
+					console.log('markkkk---')
+
+					object.children[i].geometry.computeBoundingSphere()
+
+					var pos = object.children[i].geometry.boundingSphere.center;
+					pos.add( new THREE.Vector3(0,0,0)  );
+
+
+					
+					var markObject = new THREE.AxisHelper(.5);
+					markObject.position.set( pos.x, pos.y, pos.z ) ;
+					markObject.visible = false;
+
+					// var geometry = new THREE.Geometry();
+					// geometry.vertices.push(
+					// 	new THREE.Vector3( pos.x, 0, pos.z ),
+					// 	new THREE.Vector3( pos.x, pos.y, pos.z )
+					// );
+					
+					// var material = new THREE.LineBasicMaterial( {
+					// 	color: 0xff0000,
+					// 	linewidth: 1
+					// } );
+
+					//var line = new THREE.Line( geometry, material );
+					// object.add( line );
+
+					object.add(markObject);
+
+					
+
+					object.children[i].visible = false;
 
 					var selectable = new SelectableObject();
 
-					selectable.setup( object.children[i], html_objects[i], i, this.setActive )
-					_selectableObjects.push( selectable );
+					var index =  Number(objectName[2]) - 1;
+					console.log( index, html_objects[index] )
+					selectable.setup( markObject, html_objects[index], objectName[2], this.setActive, this )
 
-					
+					_selectableObjects.push( selectable );
 				}
 
 			}// eofor
@@ -229,12 +310,12 @@ function ThreeScene(){
 
 
 		// var url = 'scene.txt'
-		var jsonLoader = new THREE.ObjectLoader();
-		jsonLoader.load('suape_clean4.json', onObjectLoad.bind(this));
+		// var jsonLoader = new THREE.ObjectLoader();
+		// jsonLoader.load('suape_clean1.json', onObjectLoad.bind(this));
 
 
-		// var loader = new THREE.OBJLoader();
-		// loader.load( 'suape_clean2.obj', onObjectLoad.bind(this) )
+		var loader = new THREE.OBJLoader();
+		loader.load( 'mapa suape_v9.obj', onObjectLoad.bind(this) )
 
 		var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
 		this.scene.add( light );
@@ -244,7 +325,6 @@ function ThreeScene(){
 		// cube.position.x = 3
 		// this.scene.add( cube );
 
-		this.camera.position.z = 10;
 
 
 
@@ -256,7 +336,6 @@ function ThreeScene(){
 
 		$(".content-close").click( function(){
 
-			console.log( "-wewe---" );
 			// $('.content-description').css({'visibility' : 'hidden' } );
 			$('.content-description').slideUp();
 		} );
@@ -265,7 +344,7 @@ function ThreeScene(){
 
 	this.draw = function (){
 
-
+		// console.log(this.camera.position);
 		// update css boxes
 		if(this.isInitialized){
 
@@ -274,7 +353,7 @@ function ThreeScene(){
 				var cameraPos = this.camera.getWorldPosition();
 				var objPos	  =  this.selectableObjects[i].object.getWorldPosition();
 
-				var alpha = ((1.0 - objPos.distanceTo(cameraPos) / 29.0) * 6);
+				var alpha = ((1.0 - objPos.distanceTo(cameraPos) / 15.0) * 6 );
 
 				if( alpha < 0.3 ){
 					alpha = 0.3;
@@ -282,16 +361,24 @@ function ThreeScene(){
 
 
 				var zIndex = Math.round(100*alpha);
+
+				if( this.selectableObjects[i].isActive  ){
+					zIndex += 1000;
+				}
+
 				var pos	= this.toScreenPosition(this.selectableObjects[i].object, this.camera);
 
-				this.selectableObjects[i].domObject.css({'top' : (pos.y - 50) + 'px', 'left': (pos.x) + 'px', 'opacity' : alpha, 'z-index': zIndex });
+				this.selectableObjects[i].domObject.css({'top' : (pos.y -70) + 'px', 'left': (pos.x - 20) + 'px', 'opacity' : alpha, 'z-index': zIndex });
 				
 			}
 		}
 
 		// smooth animation
 		var objectRotation = this.controlOrigin.rotation.y;
-		this.controlOrigin.rotation.y += (this.targetRotation - objectRotation) * 0.1;
+		// this.controlOrigin.rotation.y += (this.targetRotation - objectRotation) * 0.06;
+
+
+		this.controlOrigin.rotation.y = angleLerp(objectRotation, this.targetRotation , 0.06);
 
 
 		this.targetRotation  = this.targetRotation %  (Math.PI * 2.0);
@@ -303,7 +390,7 @@ function ThreeScene(){
 
 	}
 
-	this.setActive = function( id ){
+	this.setActive = function( id, obj, threeScene ){
 
 
 		// $('.content-description').css({'visibility' : 'hidden' } );
@@ -312,14 +399,20 @@ function ThreeScene(){
 		var idHasClass = $(id).hasClass('active');
 		var lastObj = $('.content-description.active')[0];
 
+		// this.mainObject.setRotationFromAxisAngle( new THREE.Vector3( 0, 1, 0 ), this.controls.getAzimuthalAngle() );
+		threeScene.targetRotation =   threeScene.controls.getAzimuthalAngle() - obj.targetOffset; //shortAngleDist(threeScene.controlOrigin.rotation.y, threeScene.controls.getAzimuthalAngle() + obj.targetOffset);
 
+		console.log( "Object: ", obj );
 		if( lastObj != undefined ){
 			
 			$(lastObj).removeClass( "active" );
 			$(lastObj).slideUp();
+			obj.isActive = false;
 		}
 
 		if( idHasClass == false ){
+			
+			obj.isActive = true;
 			$(id).slideDown();
 			$(id).addClass("active");
 		}
